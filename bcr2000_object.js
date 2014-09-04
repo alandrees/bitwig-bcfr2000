7484f75c2dd6ad036d095e79857f9a8ee3cc262f
Alan Drees <alandrees@theselves.com>
2014-09-04 09:36:22 -0400

    added bcr2000 object definition file to index

---
+++ bcr2000_object.js
@@ -0,0 +1,29 @@
+/**
+ * Copyright 2014 Alan Drees
+ *   
+ * Purpose:
+ *  Defines the BCR2000 controller object used to handle the BCR2000 aspect of the BCFR2000
+ * 
+ * Dependencies
+ *  bcfr2000_controls.js
+ *  bcfr2000_options.js
+ *
+ */
+
+var BCFR2000 = BCFR2000 || {};
+
+/**\fn BCFR2000.BCR2000.BCR2000Controller
+ *
+ * Constructor for the BCR2000 controller object
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+
+BCFR2000.BCR2000.BCR2000Controller = function()
+{
+
+
+}
