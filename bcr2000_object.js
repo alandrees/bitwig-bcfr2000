8704d3bc601168e91e098865ea39a9f61be95af5
Alan Drees <alandrees@theselves.com>
2014-09-04 12:00:21 -0400

    added much of the required function placeholders for the BCR controller object (init, exit, flush, etc.)

--- bcr2000_object.js
+++ bcr2000_object.js
@@ -5,14 +5,14 @@
  *  Defines the BCR2000 controller object used to handle the BCR2000 aspect of the BCFR2000
  * 
  * Dependencies
- *  bcfr2000_controls.js
+ *  bc_controls.js
  *  bcfr2000_options.js
  *
  */
 
-var BCFR2000 = BCFR2000 || {};
+var BCR = BCR || {};
 
-/**\fn BCFR2000.BCR2000.BCR2000Controller
+/**\fn BCR.BCR2000Controller
  *
  * Constructor for the BCR2000 controller object
  *
@@ -22,8 +22,98 @@ var BCFR2000 = BCFR2000 || {};
  */
 
 
-BCFR2000.BCR2000.BCR2000Controller = function()
+BCR.BCR2000Controller = function(options, instance)
+{
+    
+}
+
+
+/**\fn BCR.BCR2000Controller.prototype.init
+ *
+ * Init function to be called to initalize the 
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+
+BCR.BCR2000Controller.prototype.init = function()
 {
 
+}
 
+
+/**\fn BCR.BCR2000Controller.prototype.exit
+ *
+ * Exit function to be called when the exit event is fired
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+
+BCR.BCR2000Controller.prototype.exit = function()
+{
+
+}
+
+
+/**\fn BCR.BCR2000Controller.prototype.flush
+ *
+ * Flush function to be called when a controller's values need updating
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+
+BCR.BCR2000Controller.prototype.flush = function()
+{
+
+}
+
+
+/**\fn BCR.BCRController.prototype.onMidi
+ *
+ * Function to be fired on midi input from the controller
+ *
+ * @param status
+ * @param data1
+ * @param data2
+ *
+ * @returns None
+ */
+
+BCR.BCR2000Controller.prototype.onMidi = function(status, data1, data2)
+{
+
+}
+
+
+/**\fn BCR.BCRController.prototype.set_options
+ *
+ * Sets controller options
+ *
+ * @param options with which to set (use defaults if not set)
+ *
+ * @returns None
+ */
+
+BCR.BCR2000Controller.prototype.set_options = function(options)
+{
+    this.options = {'bcfs'          : 1, 
+		    'bcrs'          : 1,
+		    'io'            : 'bcr',
+		   };
+
+    if(typeof options === 'object')
+    {
+	for(var option in options)
+	{
+	    this.options[option] = options[option];
+	}
+    }
 }
