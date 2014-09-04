1bfebd5e3236dd836ef806375ee3913e743340d0
Alan Drees <alandrees@theselves.com>
2014-09-04 09:35:43 -0400

    added bcfr2000 control object file to index

---
+++ bcfr2000_controls.js
@@ -0,0 +1,57 @@
+/**
+ * Copyright 2014 Alan Drees
+ *   
+ * Purpose:
+ *  Implements controls used by the BCF/BCR2000   
+ *
+ * Dependencies
+ *
+ */
+
+var BCFR2000 = BCFR2000 || {};
+
+/**\fn BCFR2000.Encoder
+ *
+ * Constructor for the BCFR2000 Encoder object
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+BCFR2000.Encoder = function()
+{
+
+}
+
+
+/**\fn BCFR2000.Button
+ *
+ * Constructor for the BCFR2000 Fader object
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+BCFR2000.Button = function()
+{
+
+}
+
+
+/**\fn BCFR2000.Fader
+ *
+ * Constructor for the BCFR2000 Fader object
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+BCFR2000.Fader = function
+{
+
+}
+
+
