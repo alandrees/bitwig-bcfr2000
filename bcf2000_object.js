6efbe463e51cc48680012f36c7b40a9a05fbfe59
Alan Drees <alandrees@theselves.com>
2014-09-04 09:34:55 -0400

    added bcf2000 object definition file to index

---
+++ bcf2000_object.js
@@ -0,0 +1,31 @@
+/**
+ * Copyright 2014 Alan Drees
+ *   
+ * Purpose:
+ *  Defines the BCF2000 controller object used to handle the BCF2000 aspect of the BCFR2000
+ * 
+ * Dependencies
+ *  bcfr2000_controls.js
+ *  bcfr2000_options.js
+ *
+ */
+
+var BCFR2000 = BCFR2000 || {};
+
+if (typeof BCFR2000.BCF2000 === 'undefined') BCFR2000.BCF2000 = {};
+
+/**\fn BCFR2000.BCF2000.BCF2000Controller
+ *
+ * Constructor for the BCF2000 controller object
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+
+BCFR2000.BCF2000.BCF2000Controller = function()
+{
+
+
+}
