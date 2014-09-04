456326b196760d1f3892d23b35f79b8d0ff05040
Alan Drees <alandrees@theselves.com>
2014-09-04 11:47:20 -0400

    added much of the required function placeholders for the BCFR controller object (init, exit, flush, etc.)

--- bcfr2000_controller_object.js
+++ bcfr2000_controller_object.js
@@ -12,8 +12,10 @@
  */
 
 var BCFR2000 = BCFR2000 || {};
+if(typeof BCFR2000.BCFRController === 'undefined') BCFR2000.BCFRController = {};
 
-/**\fn BCFR2000.BCFR2000.BCFR2000Controller
+
+/**\fn BCFR2000.BCFR2000Controller
  *
  * Constructor for the BCR2000 controller object
  *
@@ -22,9 +24,107 @@ var BCFR2000 = BCFR2000 || {};
  * @returns None
  */
 
+BCFR2000.BCFRController = function(options, instance)
+{
+    this.set_options(options);
+    this.instance = instance;
 
-BCFR2000.BCFR2000.BCFR2000Controller = function()
+    this.bcr = new Array();
+    this.bcf = new Array();
+
+    for(var i = 0; i < BCFR2000.options.bcrs; i++)
+    {
+	this.bcr.push(new BCR.BCR2000Controller(options, i));
+    }
+
+    for(var i = 0; i < BCFR2000.options.bcfs; i++)
+    {
+	this.bcf.push(new BCF.BCF2000Controller(options, i));		      
+    }
+}
+
+/**\fn BCFR2000.BCFR2000Controller.prototype.init
+ *
+ * Init function to be called to initalize the 
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+BCFR2000.BCFRController.prototype.init = function()
 {
 
+}
 
+
+/**\fn BCFR2000.BCFR2000Controller.prototype.exit
+ *
+ * Exit function to be called when the exit event is fired 
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+BCFR2000.BCFRController.prototype.exit = function()
+{
+
+}
+
+/**\fn BCFR2000.BCFR2000Controller.prototype.flush
+ *
+ * Flush function to be called when a controller's values need to be updated
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+BCFR2000.BCFRController.prototype.flush = function()
+{
+
+}
+
+/**\fn BCFR2000.BCFRController.prototype.onMidi
+ *
+ * Function to be fired on midi input from the controller
+ * **THIS IS ONLY A PLACEHOLDER FUNCTION.  IT DOES NOTHING**
+ *
+ * @param status
+ * @param data1
+ * @param data2
+ *
+ * @returns None
+ */
+
+BCFR2000.BCFRController.prototype.onMidi = function(status, data1, data2)
+{
+
+}
+
+
+/**\fn BCFR2000.BCFRController.prototype.set_options
+ *
+ * Sets controller options
+ *
+ * @param options with which to set (use defaults if not set)
+ *
+ * @returns None
+ */
+
+BCFR2000.BCFRController.prototype.set_options = function(options)
+{
+    this.options = {'bcfs'          : 1, 
+		    'bcrs'          : 1,
+		    'io'            : 'bcr',
+		   };
+
+    if(typeof options === 'object')
+    {
+	for(var option in options)
+	{
+	    this.options[option] = options[option];
+	}
+    }
 }
