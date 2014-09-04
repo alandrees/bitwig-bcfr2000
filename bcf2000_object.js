ad92d8419eeb26435626f98476cdbe062be9224a
Alan Drees <alandrees@theselves.com>
2014-09-04 11:45:11 -0400

    added much of the required function placeholders for the BCF controller object (init, exit, flush, etc.)

--- bcf2000_object.js
+++ bcf2000_object.js
@@ -2,7 +2,7 @@
  * Copyright 2014 Alan Drees
  *   
  * Purpose:
- *  Defines the BCF2000 controller object used to handle the BCF2000 aspect of the BCFR2000
+ *  Defines the BCF2000 controller object
  * 
  * Dependencies
  *  bcfr2000_controls.js
@@ -10,13 +10,27 @@
  *
  */
 
-var BCFR2000 = BCFR2000 || {};
+var BCF = BCF || {};
 
-if (typeof BCFR2000.BCF2000 === 'undefined') BCFR2000.BCF2000 = {};
-
-/**\fn BCFR2000.BCF2000.BCF2000Controller
+/**\fn BCF.BCF2000Controller
  *
  * Constructor for the BCF2000 controller object
+ *
+ * @param options options to be set internally for the controller
+ * @param instace instance of the controller
+ *
+ * @returns None
+ */
+
+
+BCF.BCF2000Controller = function(options, instance)
+{
+    
+}
+
+/**\fn BCF.BCF2000Controller.prototype.init
+ *
+ * Init function to be called to initalize the 
  *
  * @param None
  *
@@ -24,8 +38,78 @@ if (typeof BCFR2000.BCF2000 === 'undefined') BCFR2000.BCF2000 = {};
  */
 
 
-BCFR2000.BCF2000.BCF2000Controller = function()
+BCF.BCF2000Controller.prototype.init = function()
 {
 
+}
 
+/**\fn BCF.BCF2000Controller.prototype.exit
+ *
+ * Exit function to be called when the exit event is fired
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+
+BCF.BCF2000Controller.prototype.exit = function()
+{
+
+}
+
+/**\fn BCF.BCF2000Controller.prototype.flush
+ *
+ * Flush function to be called when a controller's values need updating
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+
+BCF.BCF2000Controller.prototype.flush = function()
+{
+
+}
+
+/**\fn BCF.BCFController.prototype.onMidi
+ *
+ * Function to be fired on midi input from the controller
+ *
+ * @param status
+ * @param data1
+ * @param data2
+ *
+ * @returns None
+ */
+
+BCF.BCF2000Controller.prototype.onMidi = function(status, data1, data2)
+{
+
+}
+
+/**\fn BCFController.prototype.set_options
+ *
+ * Sets controller options
+ *
+ * @param options with which to set (use defaults if not set)
+ *
+ * @returns None
+ */
+
+BCF.BCF2000Controller.prototype.set_options = function(options)
+{
+    this.options = {'bcfs'          : 1, 
+		    'bcrs'          : 1,
+		    'io'            : 'bcr',
+		   };
+
+    if(typeof options === 'object')
+    {
+	for(var option in options)
+	{
+	    this.options[option] = options[option];
+	}
+    }
 }
