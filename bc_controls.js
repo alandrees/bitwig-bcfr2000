07d854a9a4eda6ba82f36c381f7a0292f913914e
Alan Drees <alandrees@theselves.com>
2014-09-04 21:28:29 -0400

    implemented most of the object functions
    - changed namespace to BC from BCFR2000
    - added BC.MIDI_MAX constant to set the upper limit on the value for a midi value
    - added a base object BC.Control for all controls to inherit from
    - added midi -> value and value -> midi conversion functions
    - added the init_value default argument to the control objects
    - implemented inheritance for each of the control objects
    - added value_in and value_out placeholder functions

--- bc_controls.js
+++ bc_controls.js
@@ -7,25 +7,122 @@
  * Dependencies
  *
  */
+var BC = BC || {};
 
-var BCFR2000 = BCFR2000 || {};
+BC.MIDI_MAX = 127; //constant for MIDI CC's.  Change this if using NRPN's or something with a higher resolution.
 
-/**\fn BCFR2000.Encoder
+/**\fn BC.Control
+ *
+ * Base object which all other BC controls inherit from
+ *
+ * @param min
+ * @param max
+ * @param control
+ * @param init_value
+ *
+ * @returns None
+ */
+
+BC.Control = function(min, max, control, init_value)
+{
+    this.value = init_value;
+    this.min   = min;
+    this.max   = max;
+    this.control = control;
+}
+
+
+/**\fn BC.Control.prototype.valueToMidi
+ *
+ * Converts an internal value to a MIDI value between 0 and 127
+ *
+ * @param None
+ *
+ * @returns midi value (between 1 and 127
+ */
+
+BC.Control.prototype.valueToMidi = function()
+{
+    return Math.round((this.value / this.max) * BC.MIDI_MAX)
+}
+
+/**\fn BC.Control.prototype.valueFromMidi
+ *
+ * Converts a MIDI value to an internal representation
+ *
+ * @param None
+ *
+ * @returns internal value rounded to the nearest integer
+ */
+
+BC.Control.prototype.valueFromMidi = function(value)
+{
+    return (value / BC.MIDI_MAX) * this.max;
+}
+
+
+/**\fn BC.Control.prototype.value_in
+ *
+ * Placeholder function for a value in callback function
+ *
+ * @param value value passed from external source
+ *
+ * @returns None
+ */
+
+BC.Control.prototype.value_in = function(value)
+{
+    println("value_in function called.  This should be over-ridden");
+}
+
+
+/**\fn BC.Control.prototype.value_out
+ *
+ * Placeholder function for a value out callback function
+ *
+ * @param value value passed from external source
+ *
+ * @returns None
+ */
+
+BC.Control.prototype.value_out = function(value)
+{
+    println("value_out function called.  This should be over-ridden");
+}
+
+
+
+
+
+/**\fn Encoder
  *
  * Constructor for the BCFR2000 Encoder object
  *
- * @param None
+ * @param max maximum value
+ * @param min minimum value
+ * @param control control value it represents
+ * @param init_value inital value (option)
  *
  * @returns None
  */
 
-BCFR2000.Encoder = function()
+BC.Encoder = function(max, min, control, init_value)
 {
-
+    if(typeof init_value === 'undefined') init_value = 0;
+    
+    BC.Control.apply(this,[max, min, control, init_value]);
 }
 
+//facilitates inheritance
+BC.Encoder.prototype = BC.Control.prototype;
+BC.Encoder.prototype.constructor = BC.Encoder;
 
-/**\fn BCFR2000.Button
+
+
+
+
+
+/**\fn Button
  *
  * Constructor for the BCFR2000 Fader object
  *
@@ -34,13 +131,18 @@ BCFR2000.Encoder = function()
  * @returns None
  */
 
-BCFR2000.Button = function()
+BC.Button = function(param, max, min, init_value)
 {
+    if(typeof init_value === 'undefined') init_value = 0;  
 
+    BC.Control.apply(this,[param, max, min, init_value]);
 }
 
+//facilitates inheritance
+BC.Button.prototype = BC.Control.prototype;
+BC.Button.prototype.constructor = BC.Button;
 
-/**\fn BCFR2000.Fader
+/**\fn Fader
  *
  * Constructor for the BCFR2000 Fader object
  *
@@ -49,9 +151,13 @@ BCFR2000.Button = function()
  * @returns None
  */
 
-BCFR2000.Fader = function()
+BC.Fader = function(param, max, min, init_value)
 {
-
+    if(typeof init_value === 'undefined') init_value = 0;
+    
+    BC.Control.apply(this,[param, max, min, init_value]);
 }
 
-
+//facilitates inheritance
+BC.Fader.prototype = BC.Control.prototype;
+BC.Fader.prototype.constructor = BC.Fader;
