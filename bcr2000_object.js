24b03f2a9126d35f311d8d20f7485926462b15e1
Alan Drees <alandrees@theselves.com>
2014-09-04 21:42:21 -0400

    many implementation changes
    - added some constructor paramters (control_builder and channel)
    - added implementation for the first version of the BCR.build_control_layout

--- bcr2000_object.js
+++ bcr2000_object.js
@@ -16,15 +16,22 @@ var BCR = BCR || {};
  *
  * Constructor for the BCR2000 controller object
  *
- * @param None
+ * @param options options to be set internally for the controller
+ * @param instace instance of the controller
+ * @param control_builder function to build the control objects
+ * @param channel channel this controller exists on
  *
  * @returns None
  */
 
 
-BCR.BCR2000Controller = function(options, instance)
+BCR.BCR2000Controller = function(options, instance, control_builder, channel)
 {
-    
+    this.set_options(options);
+    this.instance = instance;
+    this.channel = channel;
+
+    this.controls = control_builder.call(this);
 }
 
 
@@ -89,7 +96,7 @@ BCR.BCR2000Controller.prototype.flush = function()
 
 BCR.BCR2000Controller.prototype.onMidi = function(status, data1, data2)
 {
-
+    console.log('bcr midi');
 }
 
 
@@ -116,4 +123,124 @@ BCR.BCR2000Controller.prototype.set_options = function(options)
 	    this.options[option] = options[option];
 	}
     }
+}
+
+
+/**\fn BCR.build_control_layout
+ *
+ *
+ * @param None
+ *
+ * @returns None
+ */
+
+BCR.build_control_layout = function()
+{
+    var ccs = [];
+    var return_value = {};
+
+    //top row
+    ccs = [81, 82, 83, 84, 85, 86, 87, 88];
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Fader(127, 0, ccs[index], 0);
+    }
+
+    //middle row
+    ccs = [89, 90, 91, 92, 93, 94, 95, 96];
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Fader(127, 0, ccs[index], 0);
+    }
+    
+    //bottom row
+    ccs = [97, 98, 99, 100, 101, 102, 103, 104];
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Fader(127, 0, ccs[index], 0);
+    }
+
+
+    //buttons
+
+    //top row
+    ccs = [65, 66, 67, 68, 69, 70, 71, 72];
+    
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Button(127, 0, ccs[index], 0);
+    }
+
+    //bottom row
+    ccs = [79, 74, 75, 76, 77, 78, 79, 80];
+
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Button(127, 0, ccs[index], 0);
+    }
+
+
+    //the 32 grouped encoders
+    //group 1
+    ccs = [1, 2, 3, 4, 5, 6, 7, 8];
+
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Encoder(127, 0, ccs[index], 0);
+    }
+
+    //group 2
+    ccs = [9, 10, 11, 12, 13, 14, 15, 16];
+
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Encoder(127, 0, ccs[index], 0);
+    }
+
+    //group 3
+    ccs = [17, 18, 19, 20, 21, 22, 23, 24];
+    
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Encoder(127, 0, ccs[index], 0);
+    }
+
+    //group 4
+    ccs = [25, 26, 27, 28, 29, 30, 31, 32];
+
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Encoder(127, 0, ccs[index], 0);
+    }
+
+
+    //the 32 grouped buttons
+
+    //group 1
+    ccs = [33, 34, 35, 36, 37, 38, 39, 40];
+
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Button(127, 0, ccs[index], 0);
+    }
+
+    //group 2
+    ccs = [41, 42, 43, 44, 45, 46, 47, 48];
+
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Button(127, 0, ccs[index], 0);
+    }
+
+    //group 3
+    ccs = [49, 50, 51, 52, 53, 54, 55, 56];
+
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Button(127, 0, ccs[index], 0);
+    }
+
+    //group 4
+    ccs = [57, 58, 59, 60, 61, 62, 63, 64];
+
+    for(var index = 0; index < ccs.length; index++){
+	return_value[ccs[index]] = new BC.Button(127, 0, ccs[index], 0);
+    }
+
+    //User buttons
+    ccs = [105, 106, 107, 108]
+
+    for(var index = 0; index < ccs; index++){
+	return_value[ccs[index]] = new BC.Button(127, 0, ccs[index], 0);
+    }
+
+    return return_value;
 }
